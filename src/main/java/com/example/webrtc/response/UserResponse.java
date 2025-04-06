d8e3ab49ae3d1f322eaf129c23d8d01a4fb81137
Nguyen Anh <anhsnguyen1@gmail.com>
2025-04-06 12:05:04 +0700

    User

---
+++ src/main/java/com/example/webrtc/response/UserResponse.java
@@ -0,0 +1,18 @@
+package com.example.webrtc.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
+    private String id;
+    private String username;
+    private String password;
+    private String fullname;
+    private String status;
+}
